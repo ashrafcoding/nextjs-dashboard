57376c7b7fbc62530968b382d5a3eba234e909a6
ashraf <ashrafeid@outlook.com>
2024-10-28 13:59:20 +0300

    add pagination

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/app/ui/skeletons";
 
 export default async function Page() {
+  
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
